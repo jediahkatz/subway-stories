5e1939880ee82e44e6c2bfd280b65df12c9ede17
Jediah Katz <jediahkatz@gmail.com>
2024-10-09 23:54:59 -0400

    optimize total ridership query with a precomputed sum table

--- sql-server/server.js
+++ sql-server/server.js
@@ -4,11 +4,14 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Load environment variables
+require('dotenv').config();
+
 // Enable CORS for all routes
 app.use(cors());
 
 // Connect to SQLite database
-const db = new sqlite3.Database('./mta-data.db', (err) => {
+const db = new sqlite3.Database(process.env.DATABASE_PATH, (err) => {
   if (err) {
     console.error('Failed to connect to database', err);
   } else {
@@ -18,6 +21,7 @@ const db = new sqlite3.Database('./mta-data.db', (err) => {
 
 // Route 1: Ridership by Station
 app.get('/ridership-by-station', (req, res) => {
+  console.log('/ridership-by-station starts at', new Date().toISOString());
   const { selectedDay, selectedStation, selectedDirection, selectedMonths } = req.query;
   const complexId = selectedStation;
   const monthsArray = selectedMonths.split(',');
@@ -35,6 +39,7 @@ app.get('/ridership-by-station', (req, res) => {
   `;
 
   db.all(sqlQuery, [numMonthsToAverageOver, complexId, selectedDay, ...monthsArray], (err, rows) => {
+    console.log('/ridership-by-station query done at', new Date().toISOString());
     if (err) {
       res.status(500).json({ error: err.message });
     } else {
@@ -48,22 +53,19 @@ app.get('/ridership-by-station', (req, res) => {
 
 // Route 2: Total Ridership
 app.get('/total-ridership', (req, res) => {
+  console.log('/total-ridership starts at', new Date().toISOString());
+
   const { selectedDay, selectedMonths, selectedDirection } = req.query;
   const monthsArray = selectedMonths.split(',');
   const numMonthsToAverageOver = monthsArray.length;
 
   const sqlQuery = `
-    SELECT ${selectedDirection === 'goingTo' ? 'destination_complex_id' : 'origin_complex_id'} AS station_id,
-           hour_of_day AS hour,
-           SUM(estimated_avg_ridership) / ? AS total_ridership
-    FROM subway_origin_destination_2023
+    SELECT complex_id as station_id, hour_of_day as hour, total_ridership / ? as total_ridership 
+    FROM precomputed_total_ridership 
     WHERE day_of_week = ?
       AND month IN (${monthsArray.map(() => '?').join(',')})
-    GROUP BY station_id, hour
   `;
-
-  console.log('SQL Query:', sqlQuery, 'at', new Date().toISOString());
-
+  
   db.all(sqlQuery, [numMonthsToAverageOver, selectedDay, ...monthsArray], (err, rows) => {
     console.log('SQL Query done at', new Date().toISOString());
     if (err) {
