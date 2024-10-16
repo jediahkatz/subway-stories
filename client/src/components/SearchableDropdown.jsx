1afe4e579b79b191460ef13b2a50475eb7c707fc
Jediah Katz <jediahkatz@gmail.com>
2024-10-15 22:33:28 -0400

    perf fixes

--- client/src/components/SearchableDropdown.jsx
+++ client/src/components/SearchableDropdown.jsx
@@ -1,50 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useMemo, useCallback } from "react";
 
-const SubwayLineSymbol = ({ line }) => {
-  const getLineColor = (line) => {
-    // Add more color mappings as needed
-    const colors = {
-      'A': '#0039a6', 'C': '#0039a6', 'E': '#0039a6',
-      'B': '#ff6319', 'D': '#ff6319', 'F': '#ff6319', 'M': '#ff6319',
-      'G': '#6cbe45',
-      'J': '#996633', 'Z': '#996633',
-      'L': '#a7a9ac',
-      'N': '#fccc0a', 'Q': '#fccc0a', 'R': '#fccc0a', 'W': '#fccc0a',
-      '1': '#ee352e', '2': '#ee352e', '3': '#ee352e',
-      '4': '#00933c', '5': '#00933c', '6': '#00933c',
-      '7': '#b933ad',
-      'S': '#808183',
-    };
-    return colors[line] || '#000000'; // Default to black if color not found
-  };
-
-  const getLineTextColor = (line) => 
-    ['N', 'Q', 'R', 'W'].includes(line) 
-      ? '#000000' 
-      : '#ffffff';
-
-  return (
-    <span
-      style={{
-        display: 'inline-block',
-        width: '1.5em',
-        height: '1.5em',
-        borderRadius: '50%',
-        backgroundColor: getLineColor(line),
-        color: getLineTextColor(line),
-        textAlign: 'center',
-        fontWeight: 'bold',
-        marginRight: '0.5em',
-        fontSize: '0.8em',
-        lineHeight: '1.5em',
-      }}
-    >
-      {line}
-    </span>
-  );
-};
-
-const SearchableDropdown = ({
+const SearchableStationDropdown = ({
   options,
   label,
   id,
@@ -56,6 +12,12 @@ const SearchableDropdown = ({
 
   const inputRef = useRef(null);
   const dropdownRef = useRef(null);
+
+  const optionsWithLines = useMemo(() => options.map(option => {
+    const name = option[label].slice(0, option[label].lastIndexOf('(')).trim();
+    const lines = option[label].slice(option[label].lastIndexOf('(') + 1, option[label].lastIndexOf(')')).split(' ') || [];
+    return { ...option, name, lines };
+  }), [options]);
 
   useEffect(() => {
     document.addEventListener("click", toggle);
@@ -91,12 +53,22 @@ const SearchableDropdown = ({
     return selectedVal[label];
   };
 
-  const filter = (options) => {
+  const filter = useCallback((options) => {
     if (!query) return options;
-    return options.filter(
-      (option) => option[label].toLowerCase().indexOf(query.toLowerCase()) > -1
+    // If query only contains line characters, return options that include those lines
+    const queryWithNoSpaces = query.replace(/\s/g, '');
+    const queryCharSet = new Set(queryWithNoSpaces);
+    const linesSet = new Set(Object.keys(lineColors));
+    const allCharsAreLines = queryCharSet.isSubsetOf(linesSet);
+    if (allCharsAreLines) {
+      return optionsWithLines.filter(
+        (option) => option.lines.every(line => queryCharSet.has(line))
+      );
+    }
+    return optionsWithLines.filter(
+      (option) => option.name.toLowerCase().indexOf(query.toLowerCase()) > -1
     );
-  };
+  }, [query, optionsWithLines]);
 
   const handleKeyDown = (e) => {
     if (e.key === "Escape") {
@@ -105,7 +77,7 @@ const SearchableDropdown = ({
     }
   };
 
-  const renderOptionContent = (option) => {
+  const renderOptionContent = useCallback((option) => {
     const name = option[label].slice(0, option[label].lastIndexOf('(')).trim();
     const lines = option[label].slice(option[label].lastIndexOf('(') + 1, option[label].lastIndexOf(')')).split(' ') || [];
 
@@ -117,8 +89,24 @@ const SearchableDropdown = ({
         {name}
       </>
     );
-  };
+  }, [label]);
 
+  const renderedOptions = useMemo(() => {
+    return filter(options).map((option, index) => {
+          return (
+            <div
+              onClick={() => selectOption(option)}
+              className={`option ${
+                option[label] === selectedVal ? "selected" : ""
+              }`}
+              key={`${id}-${index}`}
+            >
+              {renderOptionContent(option)}
+            </div>
+          );
+    });
+  }, [options, selectedVal, id, renderOptionContent, filter]);
+  
   return (
     <div className="dropdown" ref={dropdownRef}>
       <div className="control" onClick={() => setIsOpen(!isOpen)}>
@@ -135,28 +123,61 @@ const SearchableDropdown = ({
             }}
             onClick={() => setIsOpen(true)}
             className={isOpen && query === "" ? "placeholder" : ""}
+            autoComplete="off"
           />
         </div>
         <div className={`arrow ${isOpen ? "open" : ""}`}></div>
       </div>
-
       <div className={`options ${isOpen ? "open" : ""}`}>
-        {filter(options).map((option, index) => {
-          return (
-            <div
-              onClick={() => selectOption(option)}
-              className={`option ${
-                option[label] === selectedVal ? "selected" : ""
-              }`}
-              key={`${id}-${index}`}
-            >
-              {renderOptionContent(option)}
-            </div>
-          );
-        })}
+        {renderedOptions}
       </div>
     </div>
   );
 };
 
-export default SearchableDropdown;
+const lineColors = {
+  'A': '#0039a6', 'C': '#0039a6', 'E': '#0039a6',
+  'B': '#ff6319', 'D': '#ff6319', 'F': '#ff6319', 'M': '#ff6319',
+  'G': '#6cbe45',
+  'J': '#996633', 'Z': '#996633',
+  'L': '#a7a9ac',
+  'N': '#fccc0a', 'Q': '#fccc0a', 'R': '#fccc0a', 'W': '#fccc0a',
+  '1': '#ee352e', '2': '#ee352e', '3': '#ee352e',
+  '4': '#00933c', '5': '#00933c', '6': '#00933c',
+  '7': '#b933ad',
+  'S': '#808183',
+}
+
+const SubwayLineSymbol = ({ line }) => {
+    const getLineColor = (line) => {
+      return lineColors[line] || '#000000'; // Default to black if color not found
+    };
+  
+    const getLineTextColor = (line) => 
+      ['N', 'Q', 'R', 'W'].includes(line) 
+        ? '#000000' 
+        : '#ffffff';
+  
+    return (
+      <span
+        style={{
+          display: 'inline-block',
+          width: '1.5em',
+          height: '1.5em',
+          borderRadius: '50%',
+          backgroundColor: getLineColor(line),
+          color: getLineTextColor(line),
+          textAlign: 'center',
+          fontWeight: 'bold',
+          marginRight: '0.5em',
+          fontSize: '0.8em',
+          lineHeight: '1.5em',
+        }}
+      >
+        {line}
+      </span>
+    );
+  };
+  
+
+export default SearchableStationDropdown;
