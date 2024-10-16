34f7bad4e0aa7e1dc940431a15ecea95ea8a044a
Jediah Katz <jediahkatz@gmail.com>
2024-10-15 20:23:06 -0700

    add train symbols to dropdown

--- client/src/components/SearchableDropdown.jsx
+++ client/src/components/SearchableDropdown.jsx
@@ -1,5 +1,44 @@
 import { useEffect, useRef, useState } from "react";
 
+const SubwayLineSymbol = ({ line }) => {
+  const getLineColor = (line) => {
+    // Add more color mappings as needed
+    const colors = {
+      'A': '#0039a6', 'C': '#0039a6', 'E': '#0039a6',
+      'B': '#ff6319', 'D': '#ff6319', 'F': '#ff6319', 'M': '#ff6319',
+      'G': '#6cbe45',
+      'J': '#996633', 'Z': '#996633',
+      'L': '#a7a9ac',
+      'N': '#fccc0a', 'Q': '#fccc0a', 'R': '#fccc0a', 'W': '#fccc0a',
+      '1': '#ee352e', '2': '#ee352e', '3': '#ee352e',
+      '4': '#00933c', '5': '#00933c', '6': '#00933c',
+      '7': '#b933ad',
+      'S': '#808183',
+    };
+    return colors[line] || '#000000'; // Default to black if color not found
+  };
+
+  return (
+    <span
+      style={{
+        display: 'inline-block',
+        width: '1.5em',
+        height: '1.5em',
+        borderRadius: '50%',
+        backgroundColor: getLineColor(line),
+        color: '#ffffff',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginRight: '0.5em',
+        fontSize: '0.8em',
+        lineHeight: '1.5em',
+      }}
+    >
+      {line}
+    </span>
+  );
+};
+
 const SearchableDropdown = ({
   options,
   label,
@@ -61,6 +100,20 @@ const SearchableDropdown = ({
     }
   };
 
+  const renderOptionContent = (option) => {
+    const name = option[label].split(' (')[0];
+    const lines = option[label].match(/\((.*?)\)/)?.[1]?.split(' ') || [];
+
+    return (
+      <>
+        {lines.map((line, index) => (
+          <SubwayLineSymbol key={index} line={line} />
+        ))}
+        {name}
+      </>
+    );
+  };
+
   return (
     <div className="dropdown" ref={dropdownRef}>
       <div className="control" onClick={() => setIsOpen(!isOpen)}>
@@ -92,7 +145,7 @@ const SearchableDropdown = ({
               }`}
               key={`${id}-${index}`}
             >
-              {option[label]}
+              {renderOptionContent(option)}
             </div>
           );
         })}
