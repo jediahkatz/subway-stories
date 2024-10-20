e77df10f87297a8b31871003466fdac10cb99ded
Jediah Katz <jediahkatz@gmail.com>
2024-10-20 16:39:17 -0400

    put up draft of flushing meadows

--- client/src/components/StoriesView.jsx
+++ client/src/components/StoriesView.jsx
@@ -112,12 +112,14 @@ const stories = [
           direction: 'comingFrom',
           day: 'Saturday',
           hour: 17,
-          months: [1],
+          months: [0],
+          barScale: 0.006,
         },
       },
       {
         description: <>
-          It's a different story on game day. Once the baseball season begins in late March, tens of thousands pile into Citi Field. Some fans drive or take the LIRR, but most locals jam onto the 7 train to Mets-Willets Point. They come from all over, especially nearby Queens. Two big spikes at Grand Central and the Port Authority represent the thousands who come into the city from all over the Tri-State area.
+          <p>It's a different story on game day. Once the baseball season begins in late March, tens of thousands pile into Citi Field. Some fans drive or take the LIRR, but most locals jam onto the 7 train to Mets-Willets Point.</p>
+          <p>They come from all over, especially nearby Queens. Two big spikes at Grand Central and the Port Authority represent the thousands who come into the city from all over the Tri-State area.</p>
         </>,
         // Flushing-Main St (7), Bowling Green (4 5) and 137 St-City College (1)
         pointsToInclude: [stationIdToStation['447'], stationIdToStation['414'], stationIdToStation['305']],
@@ -125,9 +127,83 @@ const stories = [
           station: '448', // Mets-Willets Point
           direction: 'comingFrom',
           day: 'Saturday',
-          hour: 15,
+          hour: 17,
           months: [4, 5, 6, 7],
+          barScale: 0.006,
         },
+        visibleLines: ['7'],
+      },
+      {
+        description: <>
+          <p>
+            In late summer, things get even more packed as the US Open kicks off. Eitan Darwish, a former ball boy, used to take the 7 to the Billie Jean King Tennis Center at 8 a.m. every morning. 
+          </p>
+          <p>
+            “The players don't take the train, they get private cars from their hotel,” he notes. “Ball boys can take an hourly shuttle, but I think the subway is faster with the traffic.” 
+          </p>
+          <p>
+            The Open has a morning session that starts at 11, and then an evening session at 7. We can see distinct spikes on the map for each session, with many fans again coming from out of town.
+          </p>
+        </>,
+        // Flushing-Main St (7), Bowling Green (4 5) and 137 St-City College (1)
+        pointsToInclude: [stationIdToStation['447'], stationIdToStation['414'], stationIdToStation['305']],
+        dataview: {
+          station: '448', // Mets-Willets Point
+          direction: 'comingFrom',
+          day: 'Saturday',
+          hour: 17,
+          months: [7, 8],
+          barScale: 0.006,
+        },
+        visibleLines: ['7'],
+      },
+      {
+        description: <>
+          <p>
+            The MTA's data is averaged over the course of the month, but major events drive enough traffic that we can pinpoint them anyway.
+          </p>
+          <p>
+            Eitan cautions, “The Mets play away games half the time, but some nights that overlap [with the US Open] happens. When it happens on a Semifinals night, the traffic is insane.”
+          </p>
+          <p>
+            One such night was Wednesday, August 30, 2023. The Mets eked out a 6-5 victory over the Rangers while Zhang Zhizhen (China) upset Casper Ruud (Norway) in the 2nd round.
+          </p>
+        </>,
+        // Flushing-Main St (7), Bowling Green (4 5) and 137 St-City College (1)
+        pointsToInclude: [stationIdToStation['447'], stationIdToStation['414'], stationIdToStation['305']],
+        dataview: {
+          station: '448', // Mets-Willets Point
+          direction: 'comingFrom',
+          day: 'Wednesday',
+          hour: 17,
+          months: [7],
+          barScale: 0.006,
+        },
+        visibleLines: ['7'],
+      },
+      {
+        description: <>
+          <p>
+            Since 2015, there's one more reason for New Yorkers to head to Flushing Meadows: Queens Night Market.
+          </p>
+          <p>
+            There, over one hundred vendors serve foods from cuisines all over the world. The market runs Saturdays from April to October on the west side of Flushing Meadows, off the 111 St stop.
+          </p>
+          <p>
+            The event is the perfect essence of Queens, the most ethnically diverse place in the world. Where else can you eat Trinidadian doubles, Mongolian yak cheese, and a Sudanese sambuxa in one meal?
+          </p>
+        </>,
+        // Flushing-Main St (7), Bowling Green (4 5) and 137 St-City College (1)
+        pointsToInclude: [stationIdToStation['447'], stationIdToStation['414'], stationIdToStation['305']],
+        dataview: {
+          station: '449', // 111 St
+          direction: 'comingFrom',
+          day: 'Saturday',
+          hour: 18,
+          months: [5, 6, 7],
+          barScale: 0.006,
+        },
+        visibleLines: ['7'],
       },
     ]
   },
@@ -252,6 +328,7 @@ const StoriesView = React.memo(({
       newSelectedDay: currentPart.dataview.day,
       newSelectedHour: currentPart.dataview.hour,
       newSelectedMonths: currentPart.dataview.months,
+      newSelectedBarScale: currentPart.dataview.barScale,
     });
     limitVisibleLines(currentPart.dataview.visibleLines);
   }, [setViewport, handleDataSettingsChange, limitVisibleLines, setCurrentStoryIndex, setCurrentPartIndex]);
