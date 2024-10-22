6fd961bdf038c2bd91899596ee17a725bbfc00d3
Jediah Katz <jediahkatz@gmail.com>
2024-10-22 03:11:15 -0400

    add ny at work story

--- client/src/components/ViewTabs.jsx
+++ client/src/components/ViewTabs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ViewTabs.css';
 
-const ViewTabs = ({ activeView, setActiveView, limitVisibleLines }) => {
+const ViewTabs = ({ activeView, setActiveView, limitVisibleLines, setSelectedBarScale }) => {
   return (
     <div className="view-tabs">
       <div className={`tab-selector ${activeView}`}>
@@ -10,6 +10,7 @@ const ViewTabs = ({ activeView, setActiveView, limitVisibleLines }) => {
           onClick={() => { 
             setActiveView('visualization')
             limitVisibleLines(null)
+            setSelectedBarScale(null)
           }}
         >
           Visualization
