f42c492b38675cac2d6b54b6aed07a221777d85e
Jediah Katz <jediahkatz@gmail.com>
2024-10-21 17:59:47 -0700

    style tooltips

--- client/src/components/SearchableDropdown.jsx
+++ client/src/components/SearchableDropdown.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState, useMemo, useCallback } from "react";
+import { subwayLines, SubwayLineSymbol } from "./SubwayLineSymbol";
 
 const SearchableStationDropdown = ({
   options,
@@ -51,7 +52,7 @@ const SearchableStationDropdown = ({
     // If query only contains line characters, return options that include those lines
     const queryWithNoSpaces = query.replace(/\s/g, '');
     const queryCharSet = new Set(queryWithNoSpaces);
-    const linesSet = new Set(Object.keys(lineColors));
+    const linesSet = new Set(subwayLines);
     const allCharsAreLines = queryCharSet.isSubsetOf(linesSet);
     if (allCharsAreLines) {
       return optionsWithLines.filter(
@@ -226,50 +227,6 @@ const SearchableStringDropdown = ({
     </div>
   );
 };
-
-const lineColors = {
-  'A': '#0039a6', 'C': '#0039a6', 'E': '#0039a6',
-  'B': '#ff6319', 'D': '#ff6319', 'F': '#ff6319', 'M': '#ff6319',
-  'G': '#6cbe45',
-  'J': '#996633', 'Z': '#996633',
-  'L': '#a7a9ac',
-  'N': '#fccc0a', 'Q': '#fccc0a', 'R': '#fccc0a', 'W': '#fccc0a',
-  '1': '#ee352e', '2': '#ee352e', '3': '#ee352e',
-  '4': '#00933c', '5': '#00933c', '6': '#00933c',
-  '7': '#b933ad',
-  'S': '#808183',
-}
-
-const SubwayLineSymbol = ({ line }) => {
-    const getLineColor = (line) => {
-      return lineColors[line] || '#000000'; // Default to black if color not found
-    };
-  
-    const getLineTextColor = (line) => 
-      ['N', 'Q', 'R', 'W'].includes(line) 
-        ? '#000000' 
-        : '#ffffff';
-  
-    return (
-      <span
-        style={{
-          display: 'inline-block',
-          width: '1.5em',
-          height: '1.5em',
-          borderRadius: '50%',
-          backgroundColor: getLineColor(line),
-          color: getLineTextColor(line),
-          textAlign: 'center',
-          fontWeight: 'bold',
-          marginLeft: '0.5em',
-          fontSize: '0.8em',
-          lineHeight: '1.5em',
-        }}
-      >
-        {line}
-      </span>
-    );
-  };
   
 
 export { SearchableStationDropdown, SearchableStringDropdown };
---
+++ client/src/components/SubwayLineSymbol.jsx
@@ -0,0 +1,36 @@
+const lineColors = {
+    'A': '#0039a6', 'C': '#0039a6', 'E': '#0039a6',
+    'B': '#ff6319', 'D': '#ff6319', 'F': '#ff6319', 'M': '#ff6319',
+    'G': '#6cbe45',
+    'J': '#996633', 'Z': '#996633',
+    'L': '#a7a9ac',
+    'N': '#fccc0a', 'Q': '#fccc0a', 'R': '#fccc0a', 'W': '#fccc0a',
+    '1': '#ee352e', '2': '#ee352e', '3': '#ee352e',
+    '4': '#00933c', '5': '#00933c', '6': '#00933c',
+    '7': '#b933ad',
+    'S': '#808183',
+}
+
+export const subwayLines = Object.keys(lineColors);
+  
+export const SubwayLineSymbol = ({ line }) => {
+    const getLineColor = (line) => {
+        return lineColors[line] || '#000000'; // Default to black if color not found
+    };
+
+    const getLineTextColor = (line) => 
+        ['N', 'Q', 'R', 'W'].includes(line) 
+            ? '#000000' 
+            : '#ffffff';
+
+    return (
+    <span className="subway-line-symbol"
+        style={{
+            backgroundColor: getLineColor(line),
+            color: getLineTextColor(line),
+        }}
+    >
+        {line}
+    </span>
+    );
+};
